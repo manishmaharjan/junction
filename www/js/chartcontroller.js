98083b9db12ed52fd7a5711e1ee8def16a3be316
Fabio <bottanfabio@hotmail.it>
2016-11-27 00:46:34 +0100

    fusion chart working

--- www/js/chartcontroller.js
+++ www/js/chartcontroller.js
@@ -3,26 +3,33 @@ angular.module('starter.controllers')
     .controller('ChartCtrl', [
         "$scope",
         function ($scope) {
-            var data = {
-                labels: ["January", "February", "March", "April", "May", "June", "July"],
-                datasets: [
+            $scope.myDataSource = {
+                chart: {
+                    caption: "Harry's SuperMart",
+                    subCaption: "Top 5 stores in last month by revenue",
+                    numberPrefix: "$",
+                    theme: "ocean"
+                },
+                data: [{
+                    label: "Bakersfield Central",
+                    value: "880000"
+                },
                     {
-                        fillColor: "rgba(220,220,220,0.5)",
-                        strokeColor: "rgba(220,220,220,1)",
-                        pointColor: "rgba(220,220,220,1)",
-                        pointStrokeColor: "#fff",
-                        data: [65, 59, 90, 81, 56, 55, 40]
+                        label: "Garden Groove harbour",
+                        value: "730000"
                     },
                     {
-                        fillColor: "rgba(151,187,205,0.5)",
-                        strokeColor: "rgba(151,187,205,1)",
-                        pointColor: "rgba(151,187,205,1)",
-                        pointStrokeColor: "#fff",
-                        data: [28, 48, 40, 19, 96, 27, 100]
-                    }
-                ]
-            }
-
-            $scope.myChart = data;
+                        label: "Los Angeles Topanga",
+                        value: "590000"
+                    },
+                    {
+                        label: "Compton-Rancho Dom",
+                        value: "520000"
+                    },
+                    {
+                        label: "Daly City Serramonte",
+                        value: "330000"
+                    }]
+            };
         }]);
 
