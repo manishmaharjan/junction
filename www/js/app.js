0371228ab33bca6c08fdc71b44191aad79a64771
Fabio <bottanfabio@hotmail.it>
2016-11-26 19:49:13 +0100

    Fix menu

--- www/js/app.js
+++ www/js/app.js
@@ -36,7 +36,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     .state('tab', {
     url: '/tab',
     abstract: true,
-    templateUrl: 'templates/tabs.html'
+    templateUrl: 'templates/menu.html'
   })
 
   // Each tab has its own nav history stack:
--- www/js/messagecontroller.js
+++ www/js/messagecontroller.js
@@ -3,4 +3,7 @@ angular.module('starter.controllers')
     .controller('MessageCtrl', [
         "$scope",
         function ($scope) {
+          $scope.startSpeechRecognition = function(){
+            console.log("ciao");
+          };
         }]);
