1eed67846c97095143d297f3f5f3a2864470858b
Fabio <bottanfabio@hotmail.it>
2016-11-26 17:42:06 +0100

    Add speech recognition directive

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.directives'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -39,70 +39,65 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     templateUrl: 'templates/tabs.html'
   })
 
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+      // Each tab has its own nav history stack:
+      .state('tab.chats', {
+        url: '/chats',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/tab-chats.html',
+            controller: 'ChatsCtrl'
+          }
         }
-      }
-    })
-
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+      })
+      .state('tab.chat-detail', {
+        url: '/chats/:chatId',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/chat-detail.html',
+            controller: 'ChatDetailCtrl'
+          }
         }
-      }
-    })
+      })
+      .state('tab.message', {
+        url: '/message',
+        views: {
+          'tab-message': {
+            templateUrl: 'templates/tab-message.html',
+            controller: 'MessageCtrl'
+          }
+        }
+      })
+      .state('tab.activity', {
+        url: '/activity',
+        views: {
+          'tab-activity': {
+            templateUrl: 'templates/tab-activity.html',
+            controller: 'ActivityCtrl'
+          }
+        }
+      })
+      .state('tab.bmicalculator', {
+        url: '/bmicalculator',
+        views: {
+          'tab-bmicalculator': {
+            templateUrl: 'templates/tab-bmicalculator.html',
+            controller: 'BmiCalculatorCtrl',
+            controllerAs: 'bmiCalcCtrl'
+          }
+        }
+      })
 
-  .state('tab.activity', {
-    url: '/activity',
-    views: {
-      'tab-activity': {
-        templateUrl: 'templates/tab-activity.html',
-        controller: 'ActivityCtrl'
-      }
-    }
-  })
+      .state('tab.account', {
+        url: '/account',
+        views: {
+          'tab-account': {
+            templateUrl: 'templates/tab-account.html',
+            controller: 'AccountCtrl'
+          }
+        }
+      });
 
-
-  .state('tab.bmicalculator', {
-    url: '/bmicalculator',
-    views: {
-      'tab-bmicalculator': {
-        templateUrl: 'templates/tab-bmicalculator.html',
-        controller: 'BmiCalculatorCtrl'
-      }
-    }
-  })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/activity');
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/activity');
 
 });
