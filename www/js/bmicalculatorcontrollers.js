f5a3f48fe8953381420fa544488969f39b790f70
Fabio <bottanfabio@hotmail.it>
2016-11-27 09:54:52 +0100

    Removed chartjs dependency in bmi calculator controller

--- www/js/bmicalculatorcontrollers.js
+++ www/js/bmicalculatorcontrollers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers')
     .controller('BmiCalculatorCtrl', [
-        "$scope",'chartjs-directive',
+        "$scope",
         function ($scope) {
             $scope.resultWeight = 0;
             $scope.numberWt = 0;
