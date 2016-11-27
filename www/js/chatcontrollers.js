43206699914b1d8a9d9363a3ac3a84e8a2ac4cf1
Manish Maharjan <manish@Manishs-MacBook-Pro.local>
2016-11-27 11:36:23 +0200

    merged

--- www/js/chatcontrollers.js
+++ www/js/chatcontrollers.js
@@ -1,7 +1,7 @@
-angular.module('starter.controllers')
+var apps = angular.module('starter.controllers')
 
 
-  .controller('ChatsCtrl', function ($scope, Chats) {
+  apps.controller('ChatsCtrl', function ($scope, Chats) {
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
     // To listen for when this page is active (for example, to refresh data),
@@ -16,6 +16,99 @@ angular.module('starter.controllers')
     };
   })
 
-  .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+  // .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+  //   $scope.chat = Chats.get($stateParams.chatId);
+  // })
+
+
+// angular.module('ionicApp', ['ionic'])
+
+// All this does is allow the message
+// to be sent when you tap return
+// .directive('input', function($timeout) {
+//   return {
+//     restrict: 'E',
+//     scope: {
+//       'returnClose': '=',
+//       'onReturn': '&',
+//       'onFocus': '&',
+//       'onBlur': '&'
+//     },
+//     link: function(scope, element, attr) {
+//       element.bind('focus', function(e) {
+//         if (scope.onFocus) {
+//           $timeout(function() {
+//             scope.onFocus();
+//           });
+//         }
+//       });
+//       element.bind('blur', function(e) {
+//         if (scope.onBlur) {
+//           $timeout(function() {
+//             scope.onBlur();
+//           });
+//         }
+//       });
+//       element.bind('keydown', function(e) {
+//         if (e.which == 13) {
+//           if (scope.returnClose) element[0].blur();
+//           if (scope.onReturn) {
+//             $timeout(function() {
+//               scope.onReturn();
+//             });
+//           }
+//         }
+//       });
+//     }
+//   }
+// })
+
+
+apps.controller('ChatDetailCtrl', function($scope, $stateParams, Chats, $ionicScrollDelegate) {
     $scope.chat = Chats.get($stateParams.chatId);
-  })
+  // $scope.hideTime = true;
+
+  var alternate,
+    isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
+
+  $scope.sendMessage = function() {
+    alternate = !alternate;
+
+    var d = new Date();
+  d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
+
+    $scope.messages.push({
+      userId: alternate ? '12345' : '54321',
+      text: $scope.data.message,
+      time: d
+    });
+
+    delete $scope.data.message;
+    $ionicScrollDelegate.scrollBottom(true);
+
+  };
+
+
+  $scope.inputUp = function() {
+    if (isIOS) $scope.data.keyboardHeight = 216;
+    $timeout(function() {
+      $ionicScrollDelegate.scrollBottom(true);
+    }, 300);
+
+  };
+
+  $scope.inputDown = function() {
+    if (isIOS) $scope.data.keyboardHeight = 0;
+    $ionicScrollDelegate.resize();
+  };
+
+  $scope.closeKeyboard = function() {
+    // cordova.plugins.Keyboard.close();
+  };
+
+
+  $scope.data = {};
+  $scope.myId = '12345';
+  $scope.messages = [];
+
+});
